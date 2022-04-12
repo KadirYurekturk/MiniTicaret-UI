3793285fbac05c0c5e9a7ddf452b3f7a46ed202a
KadirYurekturk <kadir@posta.pau.edu.tr>
2022-04-12 17:00:25 +0300

    Added links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin', component: LayoutComponent, children: [
+      { path: "customers", loadChildren: () => import('./admin/components/customers/customers.module').then(m => m.CustomersModule) },
+      { path: "", component: DashboardComponent },
+      { path: "orders", loadChildren: () => import('./admin/components/orders/orders.module').then(m => m.OrdersModule) },
+      { path: "products", loadChildren: () => import('./admin/components/products/products.module').then(m => m.ProductsModule) }
+    ]
+  },
+  { path: "", component: HomeComponent },
+  { path: "basket", loadChildren: () => import('./ui/components/basket/basket.module').then(m => m.BasketModule) },
+  { path: "products", loadChildren: () => import('./ui/components/products/products.module').then(m => m.ProductsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
