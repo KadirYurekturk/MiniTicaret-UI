2dfa36f0805f4658dffe5d651a9d3bcb8e93b404
KadirYurekturk <kadir@posta.pau.edu.tr>
2022-04-13 15:52:45 +0300

    Saved unsaved files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-declare var $: any;
+import { Injectable } from '@angular/core';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root',
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  Products: any =[];
   title = 'TicaretClient';
-  constructor(private toastr : ToastrService){
-    toastr.success('Welcome to TicaretClient', 'Welcome');
+  apiURL = "https://localhost:7249";
+  constructor(private http: HttpClient) { 
+    this.loadEmployees();
   }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      "Content-Type": "application/json",
+    })
+  }
+
+  getProducts(): Observable<Product> {
+    return this.http
+      .get<Product>(this.apiURL + "/api/products")
+      .pipe(retry(1))
+  }
+
+  loadEmployees() {
+    return this.getProducts().subscribe((data: {}) => {
+      this.Products = data;
+    });
+  }
+}
+
+export class Product {
+  Id: string;
+  Name: string;
+  Description: string;
+  Price: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UiModule } from './ui/ui.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { NgxSpinnerModule } from 'ngx-spinner';
     UiModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
